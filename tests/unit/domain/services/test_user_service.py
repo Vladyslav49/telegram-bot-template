a209d511bdd746e6b001af7b8e5ee97e215b026f
Vladyslav Timofeev <111080504+Vladyslav49@users.noreply.github.com>
2024-08-12 17:56:53 +0300

    add assign_admin_role test for UserService

--- tests/unit/domain/services/test_user_service.py
+++ tests/unit/domain/services/test_user_service.py
@@ -1,8 +1,9 @@
 import pytest
 
-from telegram_bot_template.domain.enums.user_role import UserRole
-from telegram_bot_template.domain.services.user import UserService
-from telegram_bot_template.domain.value_objects.telegram_id import TelegramId
+from parser_bot.domain.entities.user import User
+from parser_bot.domain.enums.user_role import UserRole
+from parser_bot.domain.services.user import UserService
+from parser_bot.domain.value_objects.telegram_id import TelegramId
 
 
 @pytest.fixture(scope="module")
@@ -17,3 +18,11 @@ def test_create(user_service: UserService) -> None:
 
     assert user.telegram_id == telegram_id
     assert user.role is UserRole.CLIENT
+
+
+def test_assign_admin_role(user_service: UserService) -> None:
+    user = User(telegram_id=TelegramId(1), role=UserRole.CLIENT)
+
+    user_service.assign_admin_role(user)
+
+    assert user.role is UserRole.ADMINISTRATOR
